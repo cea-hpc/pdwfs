12af2a658211115a920ea076491fc4c235949041
Julien Capul <julien.capul@cea.fr>
2019-03-14 13:56:18 +0100

    Add a development docker environment
    + fix go vet and go test targets to avoid unnecessary fetching of dependencies (they are vendored)

---
+++ docker/banner.sh
@@ -0,0 +1,8 @@
+echo "*************************************************************"
+echo "*        Welcome to pdwfs development container             *"
+echo "*                                                           *"
+echo "* To run the test suite you need a running Redis instance:  *"
+echo "*                                                           *"  
+echo "* $ scripts/pdwfs-redis start                               *"
+echo "* $ make test                                               *"
+echo "*************************************************************"
