c0987a977120486fe9830e81b0d19be9b41b8920
Julien Capul <julien.capul@cea.fr>
2019-04-29 11:35:12 +0200

    Update HydroC example to use two Redis instances

--- examples/hydroC_ParaView/run_on_pdwfs.sh
+++ examples/hydroC_ParaView/run_on_pdwfs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Start a Redis instance in background to stage data (+deactivate Redis snapshotting)
-redis-server --daemonize yes --save ""
+# Start two Redis instances in background to stage data (+deactivate Redis snapshotting)
+redis-server --port 6379 --daemonize yes --save ""
+redis-server --port 6380 --daemonize yes --save ""
+
+# Export server addresses to pdwfs
+export PDWFS_REDIS=localhost:6379,localhost:6380
 
 # Wrap the simulation command with pdwfs intercepting IO in Dep/ directory
 # Note: redirecting stderr to /dev/null as a read error appears at each iteration 
@@ -16,4 +20,5 @@ pdwfs -p Dep -- ./process_all.py
 ffmpeg -i images/test_%02d.jpg -vcodec libx264 -pix_fmt yuv420p -profile:v baseline -level 3 /output/hydro.mp4
 
 # No data to clean up on disk, just shutdown Redis
-#redis-cli shutdown
+#redis-cli -p 6379 shutdown
+#redis-cli -p 6380 shutdown
