0913556c3a5c4753e3e73a106ca370e4311dc2ee
Sebastien Morais <sebastien.morais@cea.fr>
2019-11-29 10:52:43 +0100

    [ADD] ferror test file

---
+++ src/c/tests/test_ferror.c
@@ -0,0 +1,50 @@
+/*
+* Copyright 2019 CEA
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* 	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include "tests.h"
+
+int test_ferror() {
+ 
+    FILE *f = fopen(TESTFILE, "w");
+    CHECK_NULL(f, "fopen")
+ 
+    int c = fputc('A', f);
+    assert(c == 'A');
+
+    int ret = ferror(f);
+    assert(ret == 0);
+
+    c = fgetc(f);
+    ret = ferror(f);
+    assert(ret != 0);
+
+    fclose(f);
+
+    f = fopen(TESTFILE, "r");
+    CHECK_NULL(f, "fopen")
+
+    fputc('A', f);
+    
+    ret = ferror(f);
+    assert(ret != 0);
+
+    unlink(TESTFILE);
+
+    return 0;
+}
